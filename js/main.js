c875e102958deeef88881280b39fc281087e025f
SavinDenis <116026979+SavinDenis@users.noreply.github.com>
2022-11-25 11:04:42 +0600

    full

--- js/main.js
+++ js/main.js
@@ -22,11 +22,13 @@ $(document).ready(function () {
 /* слайдер с https://kenwheeler.github.io/slick/ */
 
 $(function(){
-    $('.slider__inner').slick({
+    $('.slider__inner, .news__slider-items').slick({
         nextArrow: '<button type="button" class="slick-btn slick-next"></button>', 
         prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
         infinite: false
     });
    
 });
+
+
 
